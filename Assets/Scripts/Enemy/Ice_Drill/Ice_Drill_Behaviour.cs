13d0b394e15b66cee73dd5a50dc3740b7e2f9cab
Alexanderfzz <Alexanderfzz.haha@gmail.com>
2025-02-08 18:37:57 -0500

    feat: add Ice_Drill

---
+++ Assets/Scripts/Enemy/Ice_Drill/Ice_Drill_Behaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice_Drill_Behaviour : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPositionDelta = PlayerManager.Instance.PlayerWorldPosition - new Vector2(transform.position.x, transform.position.y);
+        float angle = Mathf.Atan2(targetPositionDelta.y, targetPositionDelta.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 targetPositionDelta = PlayerManager.Instance.PlayerWorldPosition - new Vector2(transform.position.x, transform.position.y);
+        GetComponent<Rigidbody2D>().velocity = targetPositionDelta * _moveSpeed * Time.fixedDeltaTime;
+    }
+}
